7bb1e7b92909b9d118d32d1277c38f4c9005d9bf
17045697 <17045697@myrp.edu.sg>
2019-04-25 12:41:44 +0800

    2.0

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -1,7 +1,10 @@
 package com.myapplicationdev.android.tw_listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -18,9 +21,27 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lv = findViewById(R.id.lvYears);
 
         al = new ArrayList<String>();
         al.add("Year 1");
+        al.add("Year 2");
+        al.add("Year 3");
+
+        aa = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,al);
+        lv.setAdapter(aa);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String pos = al.get(position);
+                Intent intent = new Intent(getBaseContext(),SecondActivity.class);
+                intent.putExtra("year", pos);
+                startActivity(intent);
+            }
+        });
+
 
 
 
--- app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
@@ -2,8 +2,11 @@ package com.myapplicationdev.android.tw_listview;
 
 import android.content.Context;
 
-public class ModuleAdapter {
+import java.util.ArrayList;
 
+public class ModuleAdapter extends ArrayList<Module> {
+
+    private ArrayList<Module> module;
     private Context context;
 
 
