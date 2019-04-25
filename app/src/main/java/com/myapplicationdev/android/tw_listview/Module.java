2123b31d1d42b6c9390c6d9c3676be8c47e2a9ca
shawn luah <17010564@myrp.edu.sg>
2019-04-25 12:49:43 +0800

    3.0

---
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/Module.java
@@ -0,0 +1,19 @@
+package com.myapplicationdev.android.tw_listview;
+
+public class Module {
+
+    private String moduleCode;
+    private boolean prog;
+    public Module(String name, boolean prog) {
+        this.moduleCode = moduleCode;
+        this.prog = prog;
+    }
+    public String getModuleCode() {
+        return moduleCode;
+    }
+    public boolean isProg() {
+        return prog;
+    }
+
+
+}
--- app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
@@ -1,10 +1,61 @@
 package com.myapplicationdev.android.tw_listview;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ModuleAdapter {
+import java.util.ArrayList;
 
+public class ModuleAdapter extends ArrayAdapter<Module> {
+
+    private ArrayList<Module> moduleCode;
     private Context context;
+    private TextView tvModuleCode;
+    private ImageView ivProg;
+
+    public ModuleAdapter(Context context, int resource, ArrayList<Module> objects){
+        super(context, resource, objects);
+
+        moduleCode = objects;
+
+        this.context = context;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.row, parent, false);
+
+        tvModuleCode = rowView.findViewById(R.id.tvYear);
+
+        ivProg = rowView.findViewById(R.id.ivProg);
+
+
+
+        Module currentModule = moduleCode.get(position);
+
+
+        tvModuleCode.setText(currentModule.getModuleCode());
+
+        if(currentModule.isProg()) {
+            ivProg.setImageResource(R.drawable.prog);
+        }
+        else {
+            ivProg.setImageResource(R.drawable.nonprog);
+        }
+
+        return rowView;
+    }
+
+
 
 
 
